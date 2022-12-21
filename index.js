94abb20e5d316f9c25ea9e174da5deb1b2b7f217
Akshata Kumbhar <105023659+Akshu3011@users.noreply.github.com>
2022-12-20 19:10:16 -0800

    updated the code for stripe route, changed the key

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const authRoute= require("./routes/auth")
 const productRoute= require("./routes/product")
 const cartRoute= require("./routes/cart")
 const orderRoute= require("./routes/order")
-
+const stripeRoute= require("./routes/stripe")
+const cors = require("cors")
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGO_URL)
         console.log("err")
     })
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users",userRoute)
@@ -24,6 +26,7 @@ app.use("/api/auth",authRoute)
 app.use("/api/products",productRoute)
 app.use("/api/carts",cartRoute)
 app.use("/api/orders",orderRoute)
+app.use("/api/checkout",stripeRoute)
 
 app.listen(process.env.PORT || 5000,()=>{
     console.log("background service is running on port 5000")
--- routes/stripe.js
+++ routes/stripe.js
@@ -1,5 +1,6 @@
-const router = require("express");
-const stripe = require("stripe")(process.env.STRIPE_KEY)
+const router = require("express").Router();
+const Stripe = require("stripe");
+const stripe=Stripe("sk_test_51MBv7EEiClOmVDcAhO0TSJNk8LKW7HefaCVqCSA7qmYFyTBcitZtsQ8qbxQy3iLdVsG7BAgu6kecjhBympFuIROZ00AOOdJviQ");
 
 router.post("/payment", (req,res)=>{
     stripe.charges.create({
@@ -9,6 +10,18 @@ router.post("/payment", (req,res)=>{
     },(stripeErr,stripeRes)=>{
         if(stripeErr){
             res.status(500).json(stripeErr)
+            
+                switch (stripeErr.type) {
+                  case 'StripeCardError':
+                    console.log(`A payment error occurred: ${stripeErr.message}`);
+                    break;
+                  case 'StripeInvalidRequestError':
+                    console.log('An invalid request occurred.');
+                    break;
+                  default:
+                    console.log('Another problem occurred, maybe unrelated to Stripe.');
+                    break;
+                }
         }else{
             res.status(200).json(stripeRes)
         }
